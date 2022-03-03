cf7e4c27f4f20eb17e1aa030050d8a38436a1944
reddevil141 <75828711+reddevil141@users.noreply.github.com>
2022-03-03 21:51:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function toggle1(){
+		var x = document.getElementById('ans1');
+		var y = document.getElementById('rot1');
+		y.classList.toggle('rotate');
+		x.classList.toggle('closed');
+}
+function toggle2(){
+		var x = document.getElementById('ans2');
+		var y = document.getElementById('rot2');
+		y.classList.toggle('rotate');
+		x.classList.toggle('closed');
+}function toggle3(){
+		var x = document.getElementById('ans3');
+		var y = document.getElementById('rot3');
+		y.classList.toggle('rotate');
+		x.classList.toggle('closed');
+}function toggle4(){
+		var x = document.getElementById('ans4');
+		var y = document.getElementById('rot4');
+		y.classList.toggle('rotate');
+		x.classList.toggle('closed');
+}function toggle5(){
+		var x = document.getElementById('ans5');
+		var y = document.getElementById('rot5');
+		y.classList.toggle('rotate');
+		x.classList.toggle('closed');
+}function toggle6(){
+		var x = document.getElementById('ans6');
+		var y = document.getElementById('rot6');
+		y.classList.toggle('rotate');
+		x.classList.toggle('closed');
+}
